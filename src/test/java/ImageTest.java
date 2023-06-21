de0df37ea92d9119a1af7a608b8737eafee8f786
Kamil Holody <kamilholody@live.com>
2023-06-21 12:50:18 +0200

    Lessons 219 - SeleniumTutorial

--- src/test/java/ImageTest.java
+++ src/test/java/ImageTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ImageTest {
@@ -15,10 +16,12 @@ public class ImageTest {
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        driver.get("https://testeroprogramowania.github.io/selenium/image.html");
 
         WebElement image = driver.findElement(By.tagName("img"));
-        System.out.println(image.getAttribute("naturalHeight"));
+        String height = image.getAttribute("naturalHeight");
+
+        Assert.assertEquals("0", height);
 
 
     }
