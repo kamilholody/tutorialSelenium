a351381f1b6b18a81969d6bcdd3778546c43a71d
Kamil Holody <kamilholody@live.com>
2023-06-21 12:22:57 +0200

    commit 218

---
+++ src/test/java/ImageTest.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ImageTest {
+
+    WebDriver driver;
+
+    @Test
+    public void imagetTest(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        WebElement image = driver.findElement(By.tagName("img"));
+        System.out.println(image.getAttribute("naturalHeight"));
+
+
+    }
+
+}
