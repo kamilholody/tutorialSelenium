8ea7a3ef4dbd586211c9b94a50dbf6b5d38cc18f
Kamil Holody <kamilholody@live.com>
2023-06-21 15:22:50 +0200

    Lesson 221 - Metod Sleep

---
+++ src/test/java/WaitTest.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WaitTest {
+
+    WebDriver driver;
+
+    @Test
+    public void waitTest() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
+
+        driver.findElement(By.id("clickOnMe")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("p"));
+
+    }
+
+}
