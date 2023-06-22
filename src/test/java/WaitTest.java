5b6ff06354fb40cfd630a5ed96b41c2ed8ecf9f1
Kamil Holody <kamilholody@live.com>
2023-06-22 13:53:43 +0200

    Lesson 226 - Adding own exception

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -2,13 +2,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.function.Function;
 
 
 public class WaitTest {
@@ -16,7 +18,7 @@ public class WaitTest {
     WebDriver driver;
 
     @Test
-    public void waitTest() throws InterruptedException {
+    public void waitTest()  {
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -26,14 +28,36 @@ public class WaitTest {
         driver.findElement(By.id("clickOnMe")).click();
       //  Thread.sleep(5000);
 
+
         FluentWait<WebDriver> wait = new FluentWait<>(driver);
         wait.ignoring(NoSuchElementException.class);
         wait.withTimeout(Duration.ofSeconds(10));
         wait.pollingEvery(Duration.ofSeconds(1));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("p")));
 
-        driver.findElement(By.cssSelector("p"));
+        waitForElementToExist(By.cssSelector("p"));
 
+    }
+
+    public void waitForElementToExist(By locator) {
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.pollingEvery(Duration.ofSeconds(1));
+
+        wait.until(new Function<WebDriver, Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                List<WebElement> elements = driver.findElements(locator);
+                if(elements.size()>0){
+                    System.out.println("Element jest na stronie");
+                    return true;
+                } else {
+                    System.out.println("Elementu na ma na stronie");
+                    return false;
+                }
+            }
+        });
     }
 
 }
